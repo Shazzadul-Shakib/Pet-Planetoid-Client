647a43366629b07a6bd65b9b2d33f2a74f45a31d
Shazzadul-Shakib <shakib1186@gmail.com>
2023-10-22 22:44:54 +0600

    get the value of comment

--- src/Components/Modals/CommentsModal.jsx
+++ src/Components/Modals/CommentsModal.jsx
@@ -3,9 +3,22 @@ import { IoSend } from "react-icons/io5";
 import profile from "../../assets/profile.jpg";
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
+import { useForm } from "react-hook-form";
 
 const CommentsModal = ({ post, onclose }) => {
   const { user } = useContext(AuthContext);
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  }
+
   const outsideClick = (e) => {
     if (e.target.id === "container") {
       onclose();
@@ -60,14 +73,18 @@ const CommentsModal = ({ post, onclose }) => {
             />
           </div>
           <div className=" flex-1 w-full px-4 relative">
-            <input
-              type="text"
-              placeholder="Write your comment..."
-              className=" bg-[#ffe3e3] w-full p-2 rounded-lg focus:outline-none focus:ring-0"
-            />
-            <button className="cursor-pointer">
-              <IoSend className="absolute top-3 right-7 text-gray-600 text-lg" />
-            </button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                type="text"
+                name="comment"
+                {...register("comment", { required: true })}
+                placeholder="Write your comment..."
+                className=" bg-[#ffe3e3] w-full p-2 rounded-lg focus:outline-none focus:ring-0"
+              />
+              <button type="submit" className="cursor-pointer">
+                <IoSend className="absolute top-3 right-7 text-gray-600 text-lg" />
+              </button>
+            </form>
           </div>
         </div>
       </div>
